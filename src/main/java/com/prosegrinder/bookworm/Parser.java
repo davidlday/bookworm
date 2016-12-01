690ec1d2df2655aa3e053a399f9077e8aa29ae0b
David L. Day <dday376@gmail.com>
2016-12-01 11:12:14 -0500

    Finalized Parser, still needs javadoc.

--- src/main/java/com/prosegrinder/bookworm/Parser.java
+++ src/main/java/com/prosegrinder/bookworm/Parser.java
@@ -1,21 +1,22 @@
 package com.prosegrinder.bookworm;
 
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-import java.util.Collections;
-import java.util.stream.Collectors;
-import java.util.Set;
-import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public final class Parser {
   // A Utility class for parsing prose text, specifically fiction.
   // All rules for parsing are derived from either industry practice or William Shunn's
   // "Proper Manuscript Formatting" site (//www.shunn.net/format/).
+  // TODO: I don't need to use unmodifiable things everywhere, probably just
+  // on the class variables. Even those are declared final, so probably not even there.
 
   // Regular Expression Strings
   public static final String RE_SMART_QUOTES = new String("[\u201c\u201d]");
@@ -24,50 +25,38 @@ public final class Parser {
   public static final String RE_PARAGRAPHS = new String("\\n+");
   // http://www.metaltoad.com/blog/regex-quoted-string-escapable-quotes
   // ((?<![\\])["])((?:.(?!(?<![\\])\1))*.?)\1
-  public static final String RE_DIALOGUE = new String("((?<![\\\\])[\"])((?:.(?!(?<![\\\\])\\1))*.?)\\1");
+  public static final String RE_DIALOGUE =
+      new String("((?<![\\\\])[\"])((?:.(?!(?<![\\\\])\\1))*.?)\\1");
 
-  // Point of View indicators
-//   public static final List<String> POV_FIRST = Collections.unmodifiableList(
-//     new ArrayList<String>() {{
-//       addAll("I", "I'm", "I'll", "I'd", "I've", "me", "mine", "myself");
-//       addAll("we", "we're", "we'll", "we'd", "we've");
-//       addAll("us", "ours", "ourselves");
-//     }}
-//   );
-  public static final List<String> POV_FIRST = Collections.unmodifiableList(
-    Arrays.asList(
-      "I", "I'm", "I'll", "I'd", "I've", "me", "mine", "myself",
-      "we", "we're", "we'll", "we'd", "we've",
-      "us", "ours", "ourselves"
-    )
-  );
-  public static final List<String> POV_SECOND = Collections.unmodifiableList(
-    Arrays.asList(
-      "you", "you're", "you'll", "you'd", "you've", "yours", "yourself", "yourselves"
-    )
-  );
-  public static final List<String> POV_THIRD = Collections.unmodifiableList(
-    Arrays.asList(
-      "he", "he's", "he'll", "he'd", "him", "his", "himself",
-      "she", "she's", "she'll", "she'd", "her", "hers", "herself",
-      "it", "it's", "it'll", "it'd", "itself",
-      "they", "they're", "they'll", "they'd", "they've", "them", "theirs", "themselves"
-    )
-  );
-//   public static final Map<String, String[]> POV_INDICATORS = Collections.unmodifiableMap(
-//     new HashMap<String, String[]>() {{
-//       put("first", Parser.POV_FIRST.toArray());
-//       put("second", Parser.POV_SECONDtoArray());
-//       put("third", Parser.POV_THIRDtoArray());
-//     }}
-//   );
-  public static final List<String> POV_ALL = Collections.unmodifiableList(
-    new ArrayList<String>() {{
-      addAll( Parser.POV_FIRST );
-      addAll( Parser.POV_SECOND );
-      addAll( Parser.POV_THIRD );
-    }}
-  );
+
+  // TODO: Look into moving all these POV strings to an Enum
+  public static final Set<String> POVS =
+      new HashSet<String>(Arrays.asList("first", "second", "third"));
+  public static final Set<String> POV_FIRST =
+      new HashSet<String>(Arrays.asList("I", "I'm", "I'll", "I'd", "I've", "me", "mine", "myself",
+          "we", "we're", "we'll", "we'd", "we've",
+          "us", "ours", "ourselves"));
+  public static final Set<String> POV_SECOND =
+      new HashSet<String>(Arrays.asList(
+          "you", "you're", "you'll", "you'd", "you've", "yours", "yourself", "yourselves"
+      ));
+  public static final Set<String> POV_THIRD =
+      new HashSet<String>(Arrays.asList("he", "he's", "he'll", "he'd", "him", "his", "himself",
+          "she", "she's", "she'll", "she'd", "her", "hers", "herself",
+          "it", "it's", "it'll", "it'd", "itself",
+          "they", "they're", "they'll", "they'd", "they've", "them", "theirs", "themselves"));
+  public static final Map<String, Set<String>> POV_INDICATORS =
+      new HashMap<String, Set<String>>(){{
+          put("first", Parser.POV_FIRST);
+          put("second", Parser.POV_SECOND);
+          put("third", Parser.POV_THIRD);
+      }};
+  public static final Set<String> POV_ALL =
+      new HashSet<String>(){{
+          addAll(Parser.POV_FIRST);
+          addAll(Parser.POV_SECOND);
+          addAll(Parser.POV_THIRD);
+      }};
 
   // Prohibit instantiation.
   private Parser() {
@@ -75,77 +64,106 @@ public final class Parser {
   }
 
   // Convert smart quotes to ascii quotes
-  public static final String convertQuotes(String text) {
+  public static final String convertQuotes(final String text) {
     return text.replaceAll(Parser.RE_SMART_QUOTES, "\"");
   }
 
   // Trim text and convert everything to lower case.
-  public static final String normalizeText(String text) {
+  public static final String normalizeText(final String text) {
     return text.trim().toLowerCase();
   }
 
-  // Parse a String of text into an immutable List of words.
-  public static final List<String> parseWords(String text, Boolean lowerCase) {
-    if (lowerCase)
-      text = Parser.normalizeText(text);
-    String[] words = Arrays.stream( text.split(Parser.RE_WORDS) )
-      .filter( word -> word != "" )
-      .toArray( String[]::new );
-    return Collections.unmodifiableList( Arrays.asList( words ) );
+  // Parse a String of text into a List of words.
+  public static final List<String> parseWords(final String text, final Boolean lowerCase) {
+    String localtext = text;
+    if (lowerCase) {
+      localtext = Parser.normalizeText(text);
+    }
+    String[] words = Arrays.stream(localtext.split(Parser.RE_WORDS))
+      .filter(word -> word != "")
+      .toArray(String[]::new);
+    return Arrays.asList(words);
   }
 
-  // Parse a String of text into an immutable List of words.
-  public static final List<String> parseWords(String text) {
+  // Parse a String of text into a List of words.
+  public static final List<String> parseWords(final String text) {
     return Parser.parseWords(text, true);
   }
 
-  // Parse a List of words into an immutable Set of unique words.
-  public static final Set<String> parseUniqueWords(List<String> words) {
+  // Parse a List of sentences or paragraphs into a List of words.
+  public static final List<String> parseWords(final List<String> sentences) {
+    List<String> words = new ArrayList<String>();
+    for (String sentence: sentences) {
+      words.addAll(Parser.parseWords(sentence));
+    }
+    return words;
+  }
+
+  // Parse a List of words into a Set of unique words.
+  public static final Set<String> parseUniqueWords(final List<String> words) {
     Set<String> uniqueWords = words.stream()
-		  .collect( Collectors.toSet() );
-		return Collections.unmodifiableSet( uniqueWords );
+        .collect(Collectors.toSet());
+    return uniqueWords;
   }
 
-  // Parse a String of text into an immutable Set of unique words.
-  public static final Set<String> parseUniqueWords(String text) {
-    List<String> words = Parser.parseWords( text );
-    return Parser.parseUniqueWords( words );
+  // Parse a String of text into a Set of unique words.
+  public static final Set<String> parseUniqueWords(final String text) {
+    List<String> words = Parser.parseWords(text);
+    return Parser.parseUniqueWords(words);
   }
 
-  // Parse a String of text into an immutable List of sentences.
-  public static final List<String> parseSentences(String text) {
+  // Parse a String of text into a List of sentences.
+  public static final List<String> parseSentences(final String text) {
     // Don't normalize. Case sensitive.
     String[] sentences = text.trim().split(Parser.RE_SENTENCES);
-    return Collections.unmodifiableList( Arrays.asList( sentences ) );
+    return Arrays.asList(sentences);
   }
 
-  // Parse a String of text into an immutable List of paragraphs.
-  // Paragraphs are separated by one or more new line characters.
-  public static final List<String> parseParagraphs(String text) {
-    String[] paragraphs = text.split(Parser.RE_PARAGRAPHS);
-    return Collections.unmodifiableList( Arrays.asList( paragraphs ) );
-  }
-
-  // Parse a String of text into an immutable List of dialogue.
-  // Anything between double quotation marks is considered dialogue.
-  public static final List<String> parseDialogue(String text) {
-    // http://stackoverflow.com/questions/6020384/create-array-of-regex-matches#6020436
-    Pattern p = Pattern.compile(Parser.RE_DIALOGUE);
-    Matcher m = p.matcher(text);
-    List<String> dialogue = new ArrayList<String>();
-    while (m.find()) {
-      dialogue.add( m.group().replaceAll("\"", "") );
+  // Parse a List of paragraphs into a List of sentences.
+  public static final List<String> parseSentences(final List<String> paragraphs) {
+    List<String> sentences = new ArrayList<String>();
+    for (String paragraph: paragraphs) {
+      sentences.addAll(Parser.parseSentences(paragraph));
     }
-    return Collections.unmodifiableList( dialogue );
+    return sentences;
+  }
+
+  // Parse a String of text into a List of paragraphs.
+  // Paragraphs are separated by one or more new line characters.
+  public static final List<String> parseParagraphs(final String text) {
+    // Don't normalize. Case sensitive.
+    String[] paragraphs = text.split(Parser.RE_PARAGRAPHS);
+    return Arrays.asList(paragraphs);
+  }
+
+  // Parse a String of text into a List of dialogue.
+  // Anything between double quotation marks is considered dialogue.
+  public static final List<String> parseDialogue(final String text) {
+    // http://stackoverflow.com/questions/6020384/create-array-of-regex-matches#6020436
+    Pattern pattern = Pattern.compile(Parser.RE_DIALOGUE);
+    Matcher matcher = pattern.matcher(text);
+    List<String> dialogue = new ArrayList<String>();
+    while (matcher.find()) {
+      dialogue.add(matcher.group().replaceAll("\"", ""));
+    }
+    return dialogue;
   }
 
   // Parse out an array of all POV indicators found in text.
-  public static final List<String> parsePovIndicators(String text) {
-    List<String> words = Parser.parseWords( text );
+  public static final List<String> parsePovIndicators(final String text) {
+    List<String> words = Parser.parseWords(text);
     String[] povIndicators = words.stream()
-      .filter( word -> Parser.POV_ALL.contains( word ) )
-      .toArray( String[]::new );
-    return Collections.unmodifiableList( Arrays.asList( povIndicators ) );
+      .filter(word -> Parser.POV_ALL.contains(word))
+      .toArray(String[]::new);
+    return Arrays.asList(povIndicators);
+  }
+
+  public static final List<String> parsePovIndicators(final List<String> lines) {
+    List<String> povIndicators = new ArrayList<String>();
+    for (String text: lines) {
+      povIndicators.addAll(Parser.parsePovIndicators(text));
+    }
+    return povIndicators;
   }
 
 }
