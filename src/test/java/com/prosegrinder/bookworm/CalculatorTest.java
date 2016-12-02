841987ee79aa9d58e6ed9904b5f046da6967e0b4
David L. Day <dday376@gmail.com>
2016-12-01 19:57:02 -0500

    Added shells for JUnit tests.

---
+++ src/test/java/com/prosegrinder/bookworm/CalculatorTest.java
@@ -0,0 +1,9 @@
+package com.prosegrinder.bookworm;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+
+public class CalculatorTest {
+
+}
--- src/test/java/com/prosegrinder/bookworm/ParserTest.java
+++ src/test/java/com/prosegrinder/bookworm/ParserTest.java
@@ -3,20 +3,16 @@ package com.prosegrinder.bookworm;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertArrayEquals;
 
-import java.util.Arrays;
-import java.util.List;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.Path;
-import java.io.File;
-import java.io.FileReader;
+import java.util.List;
 import org.junit.Test;
 
 public class ParserTest {
 
+  /** Need to find suitable prose for testing. **/
   private String getProse() throws IOException {
-    // http://javarevisited.blogspot.com/2015/09/how-to-read-file-into-string-in-java-7.html
     ClassLoader classLoader = this.getClass().getClassLoader();
     String prose = new String(
       Files.readAllBytes(
@@ -32,15 +28,12 @@ public class ParserTest {
   public void testParseWords() throws Exception {
     String prose = this.getProse();
     List<String> words = Parser.parseWords(prose);
-//     words.stream().forEach(System.out::println);
-//     System.out.println("Word Count: " + words.size());
   }
 
   @Test
   public void testParseDialogue() throws Exception {
     String prose = this.getProse();
     List<String> dialogue = Parser.parseDialogue(prose);
-//     dialogue.stream().forEach(System.out::println);
   }
 
 }
