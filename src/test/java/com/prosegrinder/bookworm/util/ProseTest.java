289048c46cf123b186870539d244c9a1ac38eb4e
David L. Day <dday376@gmail.com>
2017-09-04 11:25:52 -0400

    Added test for PoV indicators.

--- src/test/java/com/prosegrinder/bookworm/util/ProseTest.java
+++ src/test/java/com/prosegrinder/bookworm/util/ProseTest.java
@@ -4,6 +4,7 @@ import com.prosegrinder.bookworm.enums.PovType;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Ignore;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
@@ -11,26 +12,45 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
 public class ProseTest {
 
   private Prose prose;
+  private Prose narrativeProse;
+  private Prose dialogueProse;
 
+  /** Log4j Logger. **/
   private static final int COMPLEX_WORD_COUNT = 202;
   private static final int LONG_WORD_COUNT = 275;
   private static final int SENTENCE_COUNT = 90;
   private static final int SYLLABLE_COUNT = 2287;
   private static final int UNIQUE_WORD_COUNT = 526;
   private static final int WORD_COUNT = 1528;
+  private static final int POV_INDICATOR_COUNT = 104;
+  private static final int FIRST_PERSON_INDICATOR_COUNT = 6;
+  private static final int SECOND_PERSON_INDICATOR_COUNT = 73;
+  private static final int THIRD_PERSON_INDICATOR_COUNT = 25;
+  
 
   @Before
   public void loadProse() throws IOException, URISyntaxException {
-    String proseResource = "shunn/shortstory.txt";
+    // String proseResource = "shunn/shortstory.txt";
+    Dictionary2 dictionary = Dictionary2.getDefaultDictionary();
     ClassLoader classLoader = ProseTest.class.getClassLoader();
-    Path prosePath = Paths.get(classLoader.getResource(proseResource).toURI());
-    List<String> lines = Files.readAllLines(prosePath);
-    this.prose = new Prose(String.join("\n", lines), Dictionary2.getDefaultDictionary());
+
+    Path prosePath = Paths.get(classLoader.getResource("shunn/shortstory.txt").toURI());
+    this.prose = new Prose(String.join("\n", Files.readAllLines(prosePath)), dictionary);
+
+    Path dialogueProsePath =
+        Paths.get(classLoader.getResource("shunn/shortstory_dialogue.txt").toURI());
+    this.dialogueProse =
+        new Prose(String.join("\n", Files.readAllLines(dialogueProsePath)), dictionary);
+
+    Path narrativeProsePath =
+        Paths.get(classLoader.getResource("shunn/shortstory_narrative.txt").toURI());
+    this.narrativeProse =
+        new Prose(String.join("\n", Files.readAllLines(narrativeProsePath)), dictionary);
+
   }
 
   @Test
@@ -68,7 +88,16 @@ public class ProseTest {
 
   @Test
   public void testPov() {
+    assertEquals("Point of View internally consistent: ", narrativeProse.getPov(), prose.getPov());
     assertEquals("Point of View: ", PovType.FIRST, prose.getPov());
+  }
+
+  @Test
+  public void testPovIndicators() {
+    assertEquals("First Person PoV Indicator Count: ", ProseTest.FIRST_PERSON_INDICATOR_COUNT, prose.getFirstPersonIndicatorCount().intValue());
+    assertEquals("Second Person PoV Indicator Count: ", ProseTest.SECOND_PERSON_INDICATOR_COUNT, prose.getSecondPersonIndicatorCount().intValue());
+    assertEquals("Third Person PoV Indicator Count: ", ProseTest.THIRD_PERSON_INDICATOR_COUNT, prose.getThirdPersonIndicatorCount().intValue());
+    assertEquals("Point of View Indicator Count: ", ProseTest.POV_INDICATOR_COUNT, prose.getPovIndicatorCount().intValue());
   }
 
   @Test
@@ -76,6 +105,16 @@ public class ProseTest {
     assertEquals("Dialogue word count + narrative word count = total word count: ",
         ProseTest.WORD_COUNT,
         prose.getDialogueWordCount().intValue() + prose.getNarrativeWordCount().intValue());
+  }
+  
+  @Test
+  public void testSumWordFrequency() {
+    int sumUniqueWordCounts = 0;
+    for (Word word: prose.getUniqueWords()) {
+      sumUniqueWordCounts += prose.getWordFrequency(word).intValue();
+    }
+    assertEquals("Sum of Word Frequencies and Defined Word Count: ", ProseTest.WORD_COUNT, sumUniqueWordCounts);
+    assertEquals("Sum of Word Frequencies and Discovered Word Count: ", ProseTest.WORD_COUNT, prose.getWordCount().intValue());
   }
 
 }
